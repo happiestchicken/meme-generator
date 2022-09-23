6eefc9ff7075f16ddfc98d239132413d5d8a920a
iamkevin <kevinmcmackin1@gmail.com>
2022-09-23 18:04:01 -0300

    sept 23 work

--- src/components/main.js
+++ src/components/main.js
@@ -1,15 +1,39 @@
 import '../styles.css';
 import React from 'react';
+import memesData from "../memesData.js"
 
 function Main() {
+
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+    
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+    function getMemeImage() {
+        const memesArray = allMemeImages.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme, 
+            randomImage: url
+        }))
+    }
+
     return (
         <div className='main'>
+            <p></p>
             <div className='text-input'>
                 <input type="text" className='inputs' placeholder='Top text'></input>
                 <input  type="text" className='inputs' placeholder='Bottom text'></input>
             </div>
             <div className='create-new'>
-                <button className='button-class'>Create a new meme image</button>
+                <button onClick={getMemeImage} className='button-class'>Create a new meme image</button>
+            </div>
+            <div className='image-div'>
+                <img src={meme.randomImage} className="image"/>
             </div>
         </div>
     )
